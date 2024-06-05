40e296324e113f4597c2ec6b8727caa7e03e4278
jduncan017 <emailjoshduncan@gmail.com>
2024-06-05 04:09:30 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useRef } from "react";
 import HeroSection from "./components/hero";
-import FinalCTA from "./components/finalCTA";
-import ServicesIntro from "./components/servicesIntro";
+// import FinalCTA from "./components/finalCTA";
+// import ServicesIntro from "./components/servicesIntro";
 import ServicesOverview from "./components/servicesOverview";
 
 const Home = () => {
@@ -18,9 +18,9 @@ const Home = () => {
     <div className="Page m-auto max-w-[1728px]">
       <HeroSection scrollToRef={() => scrollToRef(heroRef)} />
       <div className="PageContent flex h-fit flex-col gap-10 px-4 py-8 sm:px-10 sm:py-20 lg:gap-20 xl:px-[144px]">
-        <ServicesIntro />
+        {/* <ServicesIntro /> */}
         {/* <ServicesOverview /> */}
-        <FinalCTA ref={heroRef} />
+        {/* <FinalCTA ref={heroRef} /> */}
       </div>
     </div>
   );
